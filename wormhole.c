5ad61dd1f63c9faea734b9761e34b10f6ea60e8d
Olaf Kirch <okir@suse.com>
2023-03-23 11:39:10 +0100

    On transactional systems, bail out early during build if /usr is readonly
    Signed-off-by: Olaf Kirch <okir@suse.com>

--- wormhole.c
+++ wormhole.c
@@ -421,6 +421,11 @@ wormhole_context_set_build_defaults(struct wormhole_context *ctx)
 			log_fatal("Unable to determine layer path for build target %s", ctx->build_target);
 	}
 
+	/* On transactional systems, where /usr is readonly, we won't be able to
+	 * create system and site layers. Catch this early on. */
+	if (!fsutil_makedirs(ctx->build_root, 0755))
+		log_fatal("Unable to initialize build root %s: %m", ctx->build_root);
+
 	if (fsutil_exists(ctx->build_root)) {
 		if (!ctx->force)
 			log_fatal("%s already exists, timidly refusing to proceed", ctx->build_root);
